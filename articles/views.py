b82c8870fee916b8b194e8fb539cd8ec20b2959c
bravin99 <bravinshumwe.coding@gmail.com>
2022-11-02 21:55:53 +0300

    Articles list view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+from django.shortcuts import redirect, render
+from django.views.generic import DeleteView, ListView
+from taggit.models import Tag
 
-# Create your views here.
+from articles.models import Article, Comments, Favourite
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class ArticleListView(ListView):
+    queryset = Article.objects.filter(publish=True)
+    template_name = "articles/articles.html"
+    context_object_name = "objects"
+    paginate_by = 12
