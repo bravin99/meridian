9e1f3559507e594320de7cefba75796e4f7ba2fa
bravin99 <bravinshumwe.coding@gmail.com>
2022-11-03 12:47:56 +0300

    contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["name", "subject", "read", "created", "updated"]
+    list_filter = ["read", "created", "updated"]
+    search_fields = ["name", "subject", "message"]
